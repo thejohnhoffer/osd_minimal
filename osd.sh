5dc3e33315faf0193823c86682ffdb2ee2b8bfd0
John Hoffer <john@hoff.in>
2018-06-20 11:58:34 -0400

    specific interest to webgl

--- osd.sh
+++ osd.sh
@@ -2,8 +2,6 @@ export clusterrank="global"
 export mclimit=500
 export ranksep=1
 
-export colorize="source colors/gitter.vim"
-vim osd_src.gv '+:source osd.vim | w !dot -Tsvg > docs/gitter.svg' '+:q!'
 export colorize="source colors/osd.vim"
 vim osd_src.gv '+:source osd.vim | w !dot -Tsvg > docs/osd.svg' '+:q!'
 export colorize="source colors/osdgl.vim"
@@ -14,8 +12,6 @@ export clusterrank="local"
 export mclimit=500
 export ranksep=3
 
-export colorize="source colors/gitter.vim"
-vim osd_src.gv '+:source osd.vim | w !dot -Tsvg > docs/gitter_class.svg' '+:q!'
 export colorize="source colors/osd.vim"
 vim osd_src.gv '+:source osd.vim | w !dot -Tsvg > docs/osd_class.svg' '+:q!'
 export colorize="source colors/osdgl.vim"
